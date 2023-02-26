3f89a00ec95f25bd9302e65e7a053cdc677e3d2b
martinamineo98 <80021258+martinamineo98@users.noreply.github.com>
2023-02-26 15:33:10 +0100

    Fixed Stylesheet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,10 @@ import { AppComponent } from './app.component';
 // NgxPaginationModule
 
 import { NgxPaginationModule } from 'ngx-pagination';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { BookPageComponent } from './book-page/book-page.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookFormComponent } from './book-form/book-form.component';
-
-
-// Ng2SearchPipeModule
-
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { BookSearchComponent } from './book-search/book-search.component';
 
 @NgModule({
